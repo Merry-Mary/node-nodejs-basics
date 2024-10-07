31aaa5466c596b970c57418437bfac7639f718ac
Mary <mary.basik@gmail.com>
2024-10-08 01:07:22 +0300

    feat: implement args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const list = [];
+    argv.forEach((arg, index) => {
+        if (arg.startsWith('--') && argv[index + 1]) {
+            list.push(`${arg.slice(2)} is ${argv[index + 1]}`);
+        }
+    });
+
+    console.log(list.join(', '));
 };
 
 parseArgs();
