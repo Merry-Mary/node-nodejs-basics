14d69d28d1a107a60baf674481b3e3e65f54d984
Mary <mary.basik@gmail.com>
2024-10-08 01:06:50 +0300

    feat: implement read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import { readFile } from 'node:fs/promises';
+import errorMessage from './common.js';
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const fileContent = await readFile('src/fs/files/fileToRead.txt', { encoding: 'utf8' });
+        console.log(fileContent);
+    } catch(error) {
+        throw new Error(errorMessage);
+    }
 };
 
 await read();
