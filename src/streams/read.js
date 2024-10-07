a608f1328129301ef599d8b156c885b1edec835d
Mary <mary.basik@gmail.com>
2024-10-08 02:09:19 +0300

    feat: implement read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'node:process';
+
+const file = fileURLToPath(import.meta.url);
+const dirName = dirname(file);
+
 const read = async () => {
-    // Write your code here 
+    const readStream = createReadStream(dirName + '/files/fileToRead.txt').setEncoding('utf8');
+    readStream.on('data', (chunk) => {
+        stdout.write(chunk);
+    });
 };
 
 await read();
