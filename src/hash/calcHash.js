9d11f5d9dda19a0e1be868ea360a436536ffcb31
Mary <mary.basik@gmail.com>
2024-10-08 02:09:30 +0300

    feat: implement calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import fs from 'node:fs';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = crypto.createHash('sha256');
+    const content = fs.createReadStream('src/hash/files/fileToCalculateHashFor.txt');
+
+    content.on('data', (chunk) => {
+        console.log(chunk)
+        process.stdout.write(crypto.createHash('sha256').update(chunk).digest('hex'));
+    });
 };
 
 await calculateHash();
