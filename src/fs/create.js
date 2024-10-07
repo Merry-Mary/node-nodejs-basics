ace6cd8fb0f2095a152149686018075024318e8e
Mary <mary.basik@gmail.com>
2024-10-08 01:04:52 +0300

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
-const create = async () => {
+import { access, writeFile } from 'node:fs/promises';
+import errorMessage from './common.js';
 
+const fileContent = 'I am fresh and young';
+
+const create = async () => {
+    await access('src/fs/files/fresh.txt').then(() => {
+        try {
+            throw new Error(errorMessage);
+        } catch(error) {
+            console.log(error)     
+        }
+    }).catch(async () => {
+        await writeFile('src/fs/files/fresh.txt', fileContent);
+    });
 };
 
 await create();
