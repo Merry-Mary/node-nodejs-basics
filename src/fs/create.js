07a54e3251f46fb7cedf2ff6cf98142447dc24dd
Mary <mary.basik@gmail.com>
2025-04-28 22:00:50 +0300

    feat: implement file system subtasks

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { access, writeFile } from 'node:fs/promises';
+import errorMessage from './common.js';
+
+const fileContent = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+    await access('src/fs/files/fresh.txt').then(() => {
+        try {
+            throw new Error(errorMessage);
+        } catch(error) {
+            console.log(error)     
+        }
+    }).catch(async () => {
+        await writeFile('src/fs/files/fresh.txt', fileContent);
+    });
 };
 
 await create();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import { readdir } from 'node:fs/promises';
+import errorMessage from './common.js';
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const files = await readdir('src/fs/files');
+        for (const file of files) {
+            console.log(file);
+        }
+    } catch(error) {
+        throw new Error(errorMessage);
+    }
 };
 
 await list();
