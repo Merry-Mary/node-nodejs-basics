ccc008ae60f3e2bf8898cf984aabeec9dd110289
Mary <mary.basik@gmail.com>
2025-04-28 22:05:28 +0300

    feat: implement worker threads subtasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import os from 'os';
+import path from 'path';
+import { Worker } from 'worker_threads';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+    const numCores = os.cpus().length;
+    const workers = [];
+
+    for (let i = 0; i < numCores; i++) {
+        workers.push(
+            new Promise((resolve) => {
+                const worker = new Worker(path.resolve(__dirname, './worker.js'));
+                worker.on('message', (msg) => {
+                    resolve(msg);
+                });
+
+                worker.on('error', () => {
+                    resolve({ status: 'error', data: null });
+                });
+
+                worker.postMessage(10 + i);
+            })
+        );
+    }
+
+    const result = await Promise.all(workers);
+    console.log(result);
 };
 
 await performCalculations();
